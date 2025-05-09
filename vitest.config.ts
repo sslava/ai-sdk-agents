6d76be9a94352874781942a042bc8862498ba2e8
Slava Sobolev <sobolevslava@gmail.com>
2025-05-09 11:55:29 +0400

    tests(fix): fix config

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    exclude: ['node_modules', 'dist', 'build'],
+    exclude: ['node_modules', 'dist', 'build', 'examples'],
     globals: true,
     environment: 'node',
   },
